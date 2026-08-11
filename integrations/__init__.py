e6d6c7e9ed908aa3591925cc5ad19d406bba6254
pavellinkee <pavellinkee@gmail.com>
2026-08-11 19:36:00 +0100

    Initialize integrations package with Telegram imports

---
+++ integrations/__init__.py
@@ -0,0 +1,10 @@
+"""
+External integrations package.
+"""
+
+from integrations.telegram_factory import (
+    TelegramTransportFactory,
+)
+from integrations.telegram_transport import (
+    TelegramBotTransport,
+)
